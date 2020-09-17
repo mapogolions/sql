ec75117bece5131302bc2d359a6001f2f65a5cd9
mapogolions <ivanovpavelalex45@gmail.com>
2020-09-17 23:58:59 +0500

    fake commit

--- views/reduce_complexity.sql
+++ views/reduce_complexity.sql
@@ -1,1 +1,10 @@
 -- TODO: add example
+
+DROP TABLE IF EXISTS [dbo].[Post]
+DROP TABLE IF EXISTS [dbo].[PostMeta]
+
+CREATE TABLE [dbo].[Post] (
+    [PostId] INT IDENTITY PRIMARY KEY,
+    [Title] NVARCHAR(100) NOT NULL,
+    [Type] NVARCHAR(100) NOT NULL
+)
