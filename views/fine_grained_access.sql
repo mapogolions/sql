3a55fcd114525261da02bb2182b065f4a28a0675
mapogolions <ivanovpavelalex45@gmail.com>
2020-09-23 22:33:24 +0500

    add the first two reason for views

--- views/fine_grained_access.sql
+++ views/fine_grained_access.sql
@@ -1,2 +1,43 @@
--- TODO: add example
--- row / column security
+DROP TABLE IF EXISTS [dbo].[Department]
+DROP TABLE IF EXISTS [dbo].[Employee]
+
+CREATE TABLE [dbo].[Department] (
+    DepartmentId INT IDENTITY PRIMARY KEY,
+    Name NVARCHAR(100) NOT NULL,
+    Location NVARCHAR(100),
+)
+
+INSERT INTO [dbo].[Department] ([Name], [Location]) VALUES ('HR', 'London'), ('IT', 'Leeds')
+
+CREATE TABLE [dbo].[Employee] (
+    EmployeeId INT IDENTITY PRIMARY KEY,
+    Name NVARCHAR(100) NOT NULL,
+    Salary DECIMAL NOT NULL,
+    DepartmentId INT FOREIGN KEY REFERENCES [dbo].[Department] ([DepartmentId])
+)
+
+INSERT INTO [dbo].[Employee] ([Name], [Salary], [DepartmentId])
+VALUES
+    ('Sam', 100, 1),
+    ('Fill', 200, 1),
+    ('Henrick', 110, 1),
+    ('Bob', 115, 2),
+    ('Mark', 100, 2),
+    ('Jim', 140, 2)
+
+DROP VIEW IF EXISTS [dbo].[vwGrantAccessITDepartment]
+
+GO
+CREATE VIEW vwGrantAccessITDepartment
+AS
+SELECT [em].[Name], [dept].[Name] as [DepartmentName] FROM [dbo].[Employee] as [em]
+INNER JOIN [dbo].[Department] as [dept]
+ON [em].[DepartmentId] = [dept].[DepartmentId]
+WHERE [dept].[Name] LIKE 'IT'
+GO
+
+-- create virtual table that contains only IT department data (row level security) & hides private info (column level security)
+SELECT * FROM vwGrantAccessITDepartment
+
+DROP TABLE IF EXISTS [dbo].[Employee]
+DROP TABLE IF EXISTS [dbo].[Department]
--- views/reduce_complexity.sql
+++ views/reduce_complexity.sql
@@ -1,10 +1,42 @@
--- TODO: add example
-
 DROP TABLE IF EXISTS [dbo].[Post]
 DROP TABLE IF EXISTS [dbo].[PostMeta]
 
 CREATE TABLE [dbo].[Post] (
-    [PostId] INT IDENTITY PRIMARY KEY,
-    [Title] NVARCHAR(100) NOT NULL,
-    [Type] NVARCHAR(100) NOT NULL
+    PostId INT IDENTITY PRIMARY KEY,
+    Title NVARCHAR(100) NOT NULL,
+    CreatedAt DATETIME NOT NULL
 )
+
+CREATE TABLE [dbo].[PostMeta] (
+    PostMetaId int IDENTITY PRIMARY KEY,
+    MetaKey NVARCHAR(100) NOT NULL,
+    MetaValue NVARCHAR(100) NOT NULL,
+    PostId INT FOREIGN KEY REFERENCES [dbo].[Post] (PostId)
+)
+
+ALTER TABLE [dbo].[PostMeta]
+ADD CONSTRAINT UQ_PostMeta_MetaKey_PostId
+UNIQUE ([MetaKey], [PostId])
+
+INSERT INTO [dbo].[Post] ([Title], [CreatedAt])
+VALUES ('AOP', GETDATE()), ('TDD', GETDATE());
+
+INSERT INTO [dbo].[PostMeta] ([MetaKey], [MetaValue], [PostId])
+VALUES ('views', '20', 1), ('likes', '1000', 1), ('views', '100', 2)
+
+DROP VIEW IF EXISTS [dbo].[vwPostMetaKeys]
+
+GO
+CREATE VIEW vwPostMetaKeys
+AS
+SELECT [post].[PostId], [Title], [MetaKey], [MetaValue] FROM [dbo].[Post] as [post]
+INNER JOIN [dbo].[PostMeta] as [postmeta]
+ON [post].[PostId] = [postmeta].[PostId]
+GO
+
+-- using views is much easier than using join (especially for non-it specialists)
+SELECT * FROM vwPostMetaKeys WHERE Title LIKE 'aop'
+SELECT * FROM vwPostMetaKeys WHERE Title LIKE 'tdd'
+
+DROP TABLE IF EXISTS [dbo].[PostMeta]
+DROP TABLE IF EXISTS [dbo].[Post]
