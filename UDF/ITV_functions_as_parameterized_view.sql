f69e59d201c3ef087b0bb3b2a8f40e0a62904d4d
mapogolions <ivanovpavelalex45@gmail.com>
2020-10-16 11:51:54 +0500

    inline table valued functions as parameterized views

---
+++ UDF/ITV_functions_as_parameterized_view.sql
@@ -0,0 +1,56 @@
+DROP TABLE IF EXISTS dbo.Post
+DROP TABLE IF EXISTS dbo.PostMeta
+
+CREATE TABLE dbo.Post (
+    PostId INT IDENTITY PRIMARY KEY,
+    Title NVARCHAR(100) NOT NULL,
+    CreatedAt DATETIME NOT NULL
+)
+
+CREATE TABLE dbo.PostMeta (
+    PostMetaId int IDENTITY PRIMARY KEY,
+    MetaKey NVARCHAR(100) NOT NULL,
+    MetaValue NVARCHAR(100) NOT NULL,
+    PostId INT FOREIGN KEY REFERENCES dbo.Post (PostId)
+)
+
+ALTER TABLE dbo.PostMeta
+ADD CONSTRAINT UQ_PostMeta_MetaKey_PostId
+UNIQUE (MetaKey, PostId)
+
+INSERT INTO dbo.Post (Title, CreatedAt)
+VALUES ('AOP', GETDATE()), ('TDD', GETDATE())
+
+INSERT INTO dbo.PostMeta (MetaKey, MetaValue, PostId)
+VALUES ('views', '20', 1), ('likes', '1000', 1), ('views', '100', 2)
+
+DROP VIEW IF EXISTS dbo.vwPostMetaKeys
+
+GO
+CREATE VIEW dbo.vwPostMetaKeys
+    AS
+    SELECT post.PostId, Title, MetaKey, MetaValue FROM dbo.Post as post
+    INNER JOIN dbo.PostMeta as postmeta
+    ON post.PostId = postmeta.PostId
+GO
+
+DROP FUNCTION IF EXISTS dbo.GetPostMetaKeys
+
+GO
+CREATE FUNCTION dbo.GetPostMetaKeys (@PostTitle NVARCHAR(100))
+    RETURNS TABLE
+AS
+    RETURN (SELECT post.PostId, Title, MetaKey, MetaValue FROM dbo.Post as post
+            INNER JOIN dbo.PostMeta as postmeta
+            ON post.PostId = postmeta.PostId
+            WHERE Title LIKE @PostTitle)
+GO
+
+
+SELECT * FROM dbo.vwPostMetaKeys WHERE Title LIKE 'aop'
+SELECT * FROM dbo.GetPostMetaKeys('aop')
+
+DROP VIEW IF EXISTS dbo.vwPostMetaKeys
+DROP FUNCTION IF EXISTS dbo.GetPostMetaKeys
+DROP TABLE IF EXISTS dbo.PostMeta
+DROP TABLE IF EXISTS dbo.Post
