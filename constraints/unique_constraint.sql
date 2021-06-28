e777e1ab17beeabe83bc08883dfda000d281b152
mapogolions <ivanovpavelalex45@gmail.com>
2021-06-28 08:39:03 -0400

    yet another approach to add unique constraint

--- constraints/unique_constraint.sql
+++ constraints/unique_constraint.sql
@@ -4,6 +4,7 @@ Create Table [dbo].[User] (
     UserId int Identity Primary Key,
     Email nvarchar(200) -- UNIQUE
     -- CONSTRAINT UQ_User_Email UNIQUE ([Email])
+    -- Unique (Email)
 )
 
 Alter Table [dbo].[User]
@@ -15,5 +16,9 @@ Select * From [dbo].[User]
 
 -- Violation of UNIQUE KEY constraint 'UQ_User_Email'. Cannot insert duplicate key in object 'dbo.User'
 -- Insert Into [dbo].[User] ([Email]) Values ('first@example.com')
+
+go
+sp_helpindex 'dbo.User'
+go
 
 Drop Table [dbo].[User]
